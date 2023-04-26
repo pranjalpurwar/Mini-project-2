01aa8092e9381bad336c5ff3be457eacc4cce045
pranjalpurwar <pranjalpurwar02@gmail.com>
2023-04-26 11:56:21 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+[Video Presentation Link](https://drive.google.com/drive/folders/1i4lHG9jRTGExtTAG0cliQXuqwWnwtn8a?usp=sharing)
---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+// Header Scroll 
+let nav = document.querySelector(".navbar");
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 50){
+        nav.classList.add("header-scrolled"); 
+        document.getElementById("navvvv").style.backgroundColor = "white";
+    }else{
+        nav.classList.remove("header-scrolled");
+        document.getElementById("navvvv").style.backgroundColor = "transparent";
+    
+    }
+}
+
+// nav hide  
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
